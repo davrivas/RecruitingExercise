93507dc8742f1058535d346e50277411fafa91ea
David Rivas <david.rivas@pevaar.com>
2023-07-19 10:56:20 -0500

    Add project files.

---
+++ RecruitingExercise/RecruitingTest.cs
@@ -0,0 +1,82 @@
+namespace RecruitingExercise
+{
+    public sealed class RecruitingTest
+    {
+        [Test]
+        public void SubscriptionsExpiredPreviousMonth()
+        {
+            var date = DateTime.Now.AddMonths(-1);
+
+            var subscriptions = new List<Subscription>
+            {
+                new(date.Month, date.Year, "Netflix"),
+                new(date.Month, date.Year, "Amazon Prime")
+            };
+
+            var user = new User(subscriptions, "Mike Mora", PaymentType.AmericanExpress);
+
+            Assert.IsTrue(user.ExpiredSubscriptions == 2, "Expected expired subscriptions: 2 - Your code returns: {0}", user.ExpiredSubscriptions);
+        }
+
+        [Test]
+        public void SubscriptionsExpiringNextMonth()
+        {
+            var date = DateTime.Now.AddMonths(1);
+
+            var subscriptions = new List<Subscription>
+            {
+                new(date.Month, date.Year, "Netflix"),
+                new(date.Month, date.Year, "Amazon Prime")
+            };
+
+            var user = new User(subscriptions, "Mike Mora", PaymentType.AmericanExpress);
+
+            Assert.IsTrue(user.ExpiredSubscriptions == 0, "Expected expired subscriptions: 0 - Your code returns: {0}", user.ExpiredSubscriptions);
+        }
+
+
+        [Test]
+        public void SubscriptionsExpiredLastYear()
+        {
+            var date = DateTime.Now.AddYears(-1);
+
+            var subscriptions = new List<Subscription>
+            {
+                new(date.Month, date.Year, "Netflix"),
+                new(date.Month, date.Year, "Amazon Prime")
+            };
+
+            var user = new User(subscriptions, "Mike Mora", PaymentType.AmericanExpress);
+
+            Assert.IsTrue(user.ExpiredSubscriptions == 2, "Expected expired subscriptions: 2 - Your code returns: {0}", user.ExpiredSubscriptions);
+        }
+
+
+        [Test]
+        public void SubscriptionsExpiredLastYearPreviousMonth()
+        {
+            Assert.Fail("Implement this test and remove this line");
+        }
+
+
+        [Test]
+        public void SubscriptionsExpiringNextYearNextMonth()
+        {
+            Assert.Fail("Implement this test and remove this line");
+        }
+
+
+        [Test]
+        public void SubscriptionsExpiringLastYearNextMonth()
+        {
+            Assert.Fail("Implement this test and remove this line");
+        }
+
+
+        [Test]
+        public void SubscriptionsExpiringNextYearPreviousMonth()
+        {
+            Assert.Fail("Implement this test and remove this line");
+        }
+    }
+}
